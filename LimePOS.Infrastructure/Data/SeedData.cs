437a574352560654741a21e86a867419ed8ce59d
Viktor.Kuleski <vkuleski@outlook.com>
2020-12-17 17:53:58 +0100

    Add project files.

---
+++ LimePOS.Infrastructure/Data/SeedData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace LimePOS.Infrastructure.Data
+{
+    public static class SeedData
+    {
+
+        public static void Initialize(IServiceProvider services)
+        {
+            //var context = services.GetRequiredService<ApplicationDbContext>();
+            //context.Database.Migrate();
+        }
+    }
+}
---
+++ LimePOS.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using LimePOS.Infrastructure.Data;
+using LimePOS.Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LimePOS.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
+            return services;
+        }
+    }
+}
